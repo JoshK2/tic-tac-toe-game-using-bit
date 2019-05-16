b6766a01f62c8581d202215fd0b51afbed1ec805
JoshK2 <yoshuakuttler@gmail.com>
2019-05-16 12:36:03 +0300

    remove dist

--- dist/src/components/Board/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Board_1 = require("./Board");
-exports.default = Board_1.default;
-//# sourceMappingURL=module.js.map
-
-//# sourceMappingURL={"version":3,"file":"module.js","sourceRoot":"","sources":["module.tsx"],"names":[],"mappings":";;AAAA,iCAAiC;AAAxB,0BAAA,OAAO,CAAA"}
--- dist/src/components/Game/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Game_1 = require("./Game");
-exports.default = Game_1.default;
-//# sourceMappingURL=module.js.map
-
-//# sourceMappingURL={"version":3,"file":"module.js","sourceRoot":"","sources":["module.tsx"],"names":[],"mappings":";;AAAA,+BAAgC;AAAvB,yBAAA,OAAO,CAAA"}
